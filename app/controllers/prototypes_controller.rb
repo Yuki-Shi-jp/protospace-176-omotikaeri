930d266902789ba5f35f8ba6fcd62dfcdfa170f0
Yuki-Shi-jp <y.shi0719@gmail.com>
2024-07-21 18:31:34 +0900

    Revert "プロトタイプが正しく表示された"
    This reverts commit 7485d94cfae0b16e00304b2a2c269303b544736c.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,4 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:edit, :show]
   def index
     @prototypes = Prototype.all
   end
@@ -13,19 +12,9 @@ class PrototypesController < ApplicationController
     redirect_to '/'
   end
 
-  def show
-  end
-
-  def edit
-  end
-
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
   end
 end
