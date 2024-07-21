d019e094b439f898f620959ab366ddaf4740390d
Yuki-Shi-jp <y.shi0719@gmail.com>
2024-07-21 14:56:16 +0900

    NewProto保存

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,5 +7,13 @@ class PrototypesController < ApplicationController
   end
 
   def create
+    Prototype.create(prototype_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
