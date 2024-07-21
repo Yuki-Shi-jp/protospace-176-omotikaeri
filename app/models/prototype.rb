8eb92c35a1b844e74cac694bd8188f990c86532d
hhiirroosshhii <hhtthhtta1@gmail.com>
2024-07-21 11:48:38 +0900

    prototypeバリデーション
    バリデーションの導入

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,10 @@
 class Prototype < ApplicationRecord
+  # バリデーション
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+
   # アソシエーション
   belong_to :user
   has_one_attached :image
