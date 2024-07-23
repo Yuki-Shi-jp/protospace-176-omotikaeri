cf4021979466c3e274ecd24af76886d29615dc82
Yuki-Shi-jp <y.shi0719@gmail.com>
2024-07-23 16:10:43 +0900

    データが保存された場合のリダイレクトとレンダーを記入した
    ただ、うまくデータベースに保存できない

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
+    if comment = Comment.create(comment_params)
+      redirect_to "/prototypes/show/#{comment.prototype_id}"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 end
